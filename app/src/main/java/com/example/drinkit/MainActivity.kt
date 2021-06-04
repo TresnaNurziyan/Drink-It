5e7cea7613f171b7781918a0eac3976d029805b7
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2021-06-04 23:12:33 +0700

    Adding FCM

--- app/src/main/java/com/example/drinkit/MainActivity.kt
+++ app/src/main/java/com/example/drinkit/MainActivity.kt
@@ -1,11 +1,93 @@
 package com.example.drinkit
 
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_retrieve_token.setOnClickListener {
+            // Get token
+            if ( checkGooglePlayServices() ) {
+                // [START retrieve_current_token]
+                FirebaseInstanceId.getInstance().instanceId
+                        .addOnCompleteListener(OnCompleteListener { task ->
+                            if (!task.isSuccessful) {
+                                Log.w(TAG, getString(R.string.token_error), task.exception)
+                                return@OnCompleteListener
+                            }
+
+                            // Get new Instance ID token
+                            val token = task.result?.token
+
+                            // Log and toast
+                            val msg = getString(R.string.token_prefix, token)
+                            Log.d(TAG, msg)
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                        })
+                // [END retrieve_current_token]
+            } else {
+                //You won't be able to send notifications to this device
+                Log.w(TAG, "Device doesn't have google play services")
+            }
+        }
+
+        val bundle = intent.extras
+        if (bundle != null) { //bundle must contain all info sent in "data" field of the notification
+            text_view_notification.text = bundle.getString("text")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver,
+                IntentFilter("MyData")
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver)
+    }
+
+    private val messageReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            text_view_notification.text = intent.extras?.getString("message")
+        }
+    }
+
+    private fun checkGooglePlayServices(): Boolean {
+        // 1
+        val status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        // 2
+        return if (status != ConnectionResult.SUCCESS) {
+            Log.e(TAG, "Error")
+            // ask user to update google play services and manage the error.
+            false
+        } else {
+            // 3
+            Log.i(TAG, "Google play services updated")
+            true
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
